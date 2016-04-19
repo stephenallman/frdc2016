f23f833cc1a4e39c914ddc4569d3fc654716df9b
Stephen Allman <stephenallman@g.harvard.edu>
2016-04-19 14:15:43 -0500

    First commit

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace FRDC2016\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace FRDC2016\Providers;
+
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'FRDC2016\Model' => 'FRDC2016\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any application authentication / authorization services.
+     *
+     * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
+     * @return void
+     */
+    public function boot(GateContract $gate)
+    {
+        $this->registerPolicies($gate);
+
+        //
+    }
+}
